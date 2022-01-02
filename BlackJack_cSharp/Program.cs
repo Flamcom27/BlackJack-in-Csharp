4dcde60f6947c2b3fe423d876af2e363fe4a16bc
Flamcom27 <27flamcom06@gmail.com>
2022-01-02 15:23:39 +0300

    class Card is created

--- BlackJack_cSharp/Program.cs
+++ BlackJack_cSharp/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlackJack_cSharp 
+{
+    public class Card
+    {
+        public dynamic rank;
+        public string suit;
+        private string name;
+        public Card(dynamic rank, string suit)
+        {
+            this.suit = suit;
+            Name = rank;
+            if (rank is char)
+            {
+                if (rank == 'A') { this.rank = new List<int>() { 1, 11 }; }
+                else { this.rank = 10; }
+            }
+            else { this.rank = rank; }
+        }
+        public dynamic Name
+        {
+            get { return this.name; }
+            set { this.name = $"{value} of {this.suit}"; }
+        }
+        public override string ToString()
+        {
+            return this.name;
+        }
+     }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = new Card(2, "Clubs");
+            var b = new Card('A', "Clubs");
+            var c = new Card('K', "Clubs");
+            foreach (var i in b.rank)
+            {
+                Console.WriteLine($" rank: {i}");
+            }
+
+        }
+    }
+}
